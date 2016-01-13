06f356273fdd39c18917a61334d0767b51ab5768
MaximeBouchenoire <bouchenoire.maxime@gmail.com>
2016-01-13 18:55:55 +0100

    Add comments on the download and install service definitions

--- githubdater-core/common/definition/IUpdateDownloader.cs
+++ githubdater-core/common/definition/IUpdateDownloader.cs
@@ -6,11 +6,29 @@ using System.Threading.Tasks;
 
 namespace NGitHubdater
 {
+    /// <summary>
+    /// Represents a service able to download an update (not installing it, see <see cref="IUpdateInstaller"/>).
+    /// </summary>
     public interface IUpdateDownloader
     {
+        /// <summary>
+        /// The name of the update downloader (have to be hard coded most of the time).
+        /// </summary>
         string Name { get; }
+
+        /// <summary>
+        /// The shorthandle of the update downloader (have to be hard coded most of the time).
+        /// </summary>
         string ShortHandle { get; }
 
+        /// <summary>
+        /// Download an update using the provided arguments.
+        /// </summary>
+        /// <param name="manifest">The update manifest.</param>
+        /// <param name="targetVersion">The release version to download.</param>
+        /// <param name="targetDirectory">The directory where downloaded files will be placed.</param>
+        /// <param name="progressCallback">The download progress callback.</param>
+        /// <returns>The result of the download process.</returns>
         DownloadResult Download(IUpdateManifest manifest, IVersion targetVersion, string targetDirectory, Action<DownloadProgress> progressCallback);
     }
 }
--- githubdater-core/common/definition/IUpdateInstaller.cs
+++ githubdater-core/common/definition/IUpdateInstaller.cs
@@ -6,11 +6,30 @@ using System.Threading.Tasks;
 
 namespace NGitHubdater
 {
+    /// <summary>
+    /// Represents a service able to install an update.
+    /// </summary>
     public interface IUpdateInstaller
     {
+        /// <summary>
+        /// The name of the update installer (have to be hard coded most of the time).
+        /// </summary>
         string Name { get; }
+
+        /// <summary>
+        /// The shorthandle of the update installer (have to be hard coded most of the time).
+        /// </summary>
         string ShortHandle { get; }
 
+        /// <summary>
+        /// Install an update using the provided arguments.
+        /// </summary>
+        /// <param name="manifest">The update manifest.</param>
+        /// <param name="files">The files to install.</param>
+        /// <param name="updateSourceDirectory">The directory where the update files are located (before installation).</param>
+        /// <param name="updateTargetDirectory">The directory where the update will be installed.</param>
+        /// <param name="progressCallback">The installation progress callback.</param>
+        /// <returns>The result of the install process.</returns>
         InstallResult Install(IUpdateManifest manifest, IEnumerable<IVersionFile> files, string updateSourceDirectory, string updateTargetDirectory, Action<IProgress> progressCallback);
     }
 }
