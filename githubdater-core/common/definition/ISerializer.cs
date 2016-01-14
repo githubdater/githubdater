c697f11655d14a01046e017b07c270411f629108
MaximeBouchenoire <bouchenoire.maxime@gmail.com>
2016-01-14 20:33:24 +0100

    Add comments on the serializer definition
    Which is defined with the ISerializer interface.

--- githubdater-core/common/definition/ISerializer.cs
+++ githubdater-core/common/definition/ISerializer.cs
@@ -6,9 +6,25 @@ using System.Threading.Tasks;
 
 namespace NGitHubdater
 {
+    /// <summary>
+    /// Represents a simple serializer
+    /// </summary>
     public interface ISerializer
     {
+        /// <summary>
+        /// Load an object from the provided path.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to load.</typeparam>
+        /// <param name="path">The path to load the object from.</param>
+        /// <returns>The loaded object.</returns>
         T Load<T>(string path);
+
+        /// <summary>
+        /// Save an object to the provided path.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to save.</typeparam>
+        /// <param name="path">The path where the object will be saved.</param>
+        /// <param name="obj">The object to save.</param>
         void Save<T>(string path, T obj);
     }
 }
