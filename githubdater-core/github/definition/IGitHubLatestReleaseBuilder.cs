5a6ecef5c14ec51e2f62425e52d4253c55a6027e
MaximeBouchenoire <bouchenoire.maxime@gmail.com>
2016-01-25 23:11:17 +0100

    Add comments on GitHub releases builders definitions

--- githubdater-core/github/definition/IGitHubLatestReleaseBuilder.cs
+++ githubdater-core/github/definition/IGitHubLatestReleaseBuilder.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace NGitHubdater
 {
+    /// <summary>
+    /// Represents a service able to build a <see cref="GitHubRelease"/> from a given URL and a <see cref="GitHubUpdateManifest"/>.
+    /// </summary>
     interface IGitHubLatestReleaseBuilder
     {
+        /// <summary>
+        /// Build a <see cref="GitHubRelease"/> from a given URL and a <see cref="GitHubUpdateManifest"/>.
+        /// </summary>
+        /// <param name="releasesUrl">The url where the releases are listed.</param>
+        /// <param name="manifest">The update manifest used to determine which releases should be retrieved.</param>
+        /// <returns></returns>
         GitHubRelease build(string releasesUrl, GitHubUpdateManifest manifest);
     }
 }
--- githubdater-core/github/definition/IHtmlGitHubReleaseBuilder.cs
+++ githubdater-core/github/definition/IHtmlGitHubReleaseBuilder.cs
@@ -7,8 +7,18 @@ using System.Threading.Tasks;
 
 namespace NGitHubdater
 {
+    /// <summary>
+    /// Represents a service able to build a <see cref="GitHubRelease"/> from it's HTML.
+    /// </summary>
     interface IHtmlGitHubReleaseBuilder
     {
+        /// <summary>
+        /// Builds a <see cref="GitHubRelease"/> from it's HTML and an update manifest.
+        /// </summary>
+        /// <param name="releaseHtml">The release HTML (from it's GitHub page).</param>
+        /// <param name="preRelease">Wether or not the release to build is a pre-release. This can't be retrieved from it's inner HTML.</param>
+        /// <param name="manifest">The update manifest.</param>
+        /// <returns>The <see cref="GitHubRelease"/> parsed from it's GitHub HTML.</returns>
         GitHubRelease build(string releaseHtml, bool preRelease, GitHubUpdateManifest manifest);
     }
 }
